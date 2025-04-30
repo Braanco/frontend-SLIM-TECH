25567c2cd2607bda256f930f6f5ea8606c75eec8
AntonioMrcosDev <antonio_marcos000@hotmail.com>
2025-04-29 21:04:20 -0300

    criação do service.js, e atualizaçao do html

---
+++ service.js
@@ -0,0 +1,42 @@
+const urlbase = "http://localhost:8080/slim_tech/form"
+
+document.getElementById("btn_enviar").addEventListener("click", function(event){
+    event.preventDefault();
+    const datas = {
+        altura : document.getElementById("altura").value , 
+        peso : document.getElementById("peso").value , 
+        genero : document.getElementById("genero").value.toUpperCase() , 
+        idade : document.getElementById("idade").value  , 
+        problemaDeSaude : document.getElementById("problemas_saude").value , 
+        metaDoUsuario : document.getElementById("meta_usuario").value 
+    }
+    sendData(datas);
+})
+
+async function sendData(datas) {
+    const loading = document.getElementById("loading");
+    loading.style.display = "flex";
+    try {
+        const response = await fetch(urlbase, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(datas)
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            alert("Erro na hora de processar");
+            throw new Error(errorData.message || "Erro no request");
+        }
+
+        const data = await response.json();
+        alert("Sucesso!");
+        console.log('resposta salva:', data);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        loading.style.display = "none"; // sempre esconde o loading
+    }
+}
